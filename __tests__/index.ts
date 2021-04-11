a39f96009f103ab3f6a2fa4c647399e100f77f49
Dmitry Smurygin <d.a.smurygin@gmail.com>
2021-04-11 09:20:43 +0300

    feat: initial implementation of getPaths method

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,9 +1,57 @@
 import {test} from 'uvu'
-import {type} from 'uvu/assert'
-import {getPaths} from '../src'
+import {equal, is, type} from 'uvu/assert'
+import {getPaths, TinyPathGetPaths, TinyPaths} from '../src'
+
+interface ITestCaseObj {
+	propRoot: string;
+	propContainerLevel1: {
+		propInsideLevel1: string;
+		propContainerLevel2: {
+			propInsideLevel2: string;
+		}
+	}
+}
+
+const setupTestCase = (): [TinyPaths<ITestCaseObj>, ReturnType<TinyPathGetPaths>] => [
+	{
+		propRoot: 'propRoot',
+		propContainerLevel1: {
+			propInsideLevel1: 'propInsideLevel1',
+			propContainerLevel2: {
+				propInsideLevel2: 'propInsideLevel2'
+			}
+		}
+	},
+	{
+		propRoot: 'propRoot',
+		propContainerLevel1: {
+			self: 'propContainerLevel1',
+			propInsideLevel1: 'propContainerLevel1.propInsideLevel1',
+			propContainerLevel2: {
+				self: 'propContainerLevel1.propContainerLevel2',
+				propInsideLevel2: 'propContainerLevel1.propContainerLevel2.propInsideLevel2'
+			}
+		}
+	}
+]
 
 test('tinypath', () => {
 	type(getPaths, 'function', 'exports a function')
+})
+
+test('should return an object with the same structure, but with values that are the full path to the full path to each property', () => {
+	const [object, expected] = setupTestCase()
+	const objectPath = getPaths<ITestCaseObj>(object)
+
+	equal(objectPath, expected)
+})
+
+test('should return the path to the property which contains an object. In this case, it is can be obtained by the "self" keyword.', () => {
+	const [object] = setupTestCase()
+	const objectPath = getPaths<ITestCaseObj>(object)
+
+	is(objectPath.propContainerLevel1.self, 'propContainerLevel1')
+	is(objectPath.propContainerLevel1.propContainerLevel2.self, 'propContainerLevel1.propContainerLevel2')
 })
 
 test.run()
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
-const getPaths = () => {
-	//
+type Primitives = string | number | boolean
+type DeepPartial<T extends {}> = {
+    [P in keyof T]?: T[P] extends {} ? DeepPartial<T[P]> : T[P]
+}
+export declare type TinyPaths<T extends {}, TPropsWithOnlySelfProperty extends string = never, TPath extends string = never> = DeepPartial<{
+    [P in keyof T]:
+    P extends TPropsWithOnlySelfProperty ? P :
+        T[P] extends Array<unknown> ? P :
+            T[P] extends Primitives ? P : (TinyPaths<T[P]> & { self: P })
+}>
+export declare type TinyPathGetPaths = <TObject extends {}>(paths: TinyPaths<TObject>) => TinyPaths<TObject>
+const getPaths: TinyPathGetPaths = <TObject extends {}>(obj) => {
+    const iterate = (
+        obj: TObject,
+        base: string
+    ) => Object.keys(obj)
+        .reduce((acc, value) => {
+            const path = base ? `${base}.${value}` : value
+            return typeof obj[value] !== 'object'
+                ? value === 'self'
+                    ? acc as ReturnType<TinyPathGetPaths>
+                    : {...acc as ReturnType<TinyPathGetPaths>, [value]: path}
+                : {
+                    ...acc as ReturnType<TinyPathGetPaths>,
+                    [value]: {...iterate(obj[value], path), self: path}
+                }
+        }, {} as ReturnType<TinyPathGetPaths>)
+    return iterate(obj, '')
 }
 export {getPaths}
